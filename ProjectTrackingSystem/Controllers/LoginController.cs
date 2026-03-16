ddd2061fb901e55944405980250daaac07524eb1
yaren0600 <begumozturk0600@gmail.com>
2026-03-17 02:40:48 +0300

    Implemented a complete user login flow by updating the LoginController with session management and database validation while refactoring the Index.cshtml view to use Html.BeginForm for secure data submission.

--- ProjectTrackingSystem/Controllers/LoginController.cs
+++ ProjectTrackingSystem/Controllers/LoginController.cs
@@ -16,5 +16,40 @@ namespace ProjectTrackingSystem.Controllers
         {
             return View();
         }
+
+        [HttpPost]// Handle the login form submission
+        public ActionResult Index(string username, string password)
+        {
+            var user = (from u in entity.Users
+                        where u.userName == username && u.userPassword == password
+                        select u).FirstOrDefault(); // Query the database for a user with the provided username and password
+            if(user != null)
+            {
+                // session variables to store user information for the duration of the user's session
+                Session["userFullName"] = user.userFullName; // Store the user's full name in the session
+                Session["userId"] = user.userId; // Store the user's ID in the session
+                Session["userClassId"] = user.userClassId; // Store the user's class ID in the session
+                Session["userRoleId"] = user.userRoleId; // Store the user's role ID in the session
+
+                switch (user.userRoleId) // Redirect the user to different pages based on their role ID
+                {
+                    case 1: // If the user is a project manager
+                        return RedirectToAction("ProjectManagerDashboard", "ProjectManager"); // Redirect to the project manager dashboard
+                    case 2: // If the user is a team member
+                        return RedirectToAction("TeamMemberDashboard", "TeamMember"); // Redirect to the team member dashboard
+                    default:
+                        break;
+                }
+                return RedirectToAction("Index", "Home"); // Redirect to the home page if login is successful
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid username or password"; // Display an error message if login fails
+                return View(); // Return the login view to allow the user to try again
+            }
+               
+           
+        }
+        
     }
 }
